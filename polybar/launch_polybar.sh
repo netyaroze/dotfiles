cacd909205c32af585725c84e1a5bcb837d7b04c
Jeremy Cocks <jeremy.cocks@forgerock.com>
2020-09-08 23:41:55 +0100

    change for laptop

---
+++ polybar/launch_polybar.sh
@@ -0,0 +1,7 @@
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload nixyaroze &
+  done
+else
+  polybar --reload example &
+fi
